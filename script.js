b4201b07ed9f98dbf7851e9c5b31537d0884bb17
Kenechukwu <anyambakenneth@gmail.com>
2023-12-12 21:33:24 +0100

    user interface of contact manager

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const viewList = document.querySelector(".viewList");
+const newContact = document.querySelector(".newContact");
+const viewContact = document.querySelector(".viewContact");
+
+//View Contact displays contact list
+viewList.addEventListener("click", function () {});
+//clicking view beside contact displays contact details
+//Delete button deletes contact
+//Add new displays form to add new contact
